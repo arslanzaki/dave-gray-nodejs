4fcb8764afe1e359f265a7d9e2cf417826a254c7
Arslan Zaki <59029560+arslanzaki@users.noreply.github.com>
2023-02-14 22:14:52 +0500

    JWT Auth

---
+++ 11TUT/controllers/logoutController.js
@@ -0,0 +1,46 @@
+const usersDB = {
+  users: require("../model/users.json"),
+  setUsers: function (data) {
+    this.users = data;
+  },
+};
+
+//   const jwt = require("jsonwebtoken");
+//   require("dotenv").config();
+const fsPromises = require("fs").promises;
+const path = require("path");
+
+const handleLogout = async (req, res) => {
+  // On Client, Also Delete The accessToken
+  const cookies = req.cookies;
+  if (!cookies?.jwt) {
+    return res.sendStatus(204); // No Content
+  }
+  const refreshToken = cookies.jwt;
+  // Is refreshToken In DB?
+  const foundUser = usersDB.users.find(
+    (person) => person.refreshToken === refreshToken
+  );
+  if (!foundUser) {
+    res.clearCookie("jwt", { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 });
+    return res.sendStatus(204);
+  }
+
+  // Delete refreshToken In DB
+
+  const otherUsers = usersDB.users.filter(
+    (person) => person.refreshToken !== foundUser.refreshToken
+  );
+  const currentUser = { ...foundUser, refreshToken: "" };
+  usersDB.setUsers([...otherUsers, currentUser]);
+
+  await fsPromises.writeFile(
+    path.join(__dirname, "..", "model", "users.json"),
+    JSON.stringify(usersDB.users)
+  );
+
+  res.clearCookie("jwt", { httpOnly: true, sameSite: 'None', secure: true});
+  // secure: true - only serves on https
+  res.sendStatus(204);
+};
+module.exports = { handleLogout };
