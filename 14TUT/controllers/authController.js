c9dd4d16860e24c054eb050747d78beda03e5821
Arslan Zaki <59029560+arslanzaki@users.noreply.github.com>
2023-02-18 11:28:08 +0500

    Final Touches

--- 14TUT/controllers/authController.js
+++ 14TUT/controllers/authController.js
@@ -23,7 +23,7 @@ const handleLogin = async (req, res) => {
         },
       },
       process.env.ACCESS_TOKEN_SECRET,
-      { expiresIn: "30s" }
+      { expiresIn: "55s" }
     );
     const refreshToken = jwt.sign(
       { username: foundUser.username },
